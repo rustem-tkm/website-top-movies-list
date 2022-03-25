faebc46bb688320ea1cef60afb16108b10321a51
Rustem Tkm <63965587+rustem-tkm@users.noreply.github.com>
2022-03-25 18:10:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 Bootstrap(app)
 
-MOVIE_DB_API_KEY = "d13b07dedd8978c3c206fde6e2cf9ee8"
+MOVIE_DB_API_KEY = "YOUR_API_KEY"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
